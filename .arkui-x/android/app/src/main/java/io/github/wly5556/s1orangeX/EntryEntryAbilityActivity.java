aee882a1ebf8cbcc20e97cd217157b24ba546b5c
wly5556 <61644753+wly5556@users.noreply.github.com>
2025-02-23 23:05:04 +0800

    移植到ArkUI-X

---
+++ .arkui-x/android/app/src/main/java/io/github/wly5556/s1orangeX/EntryEntryAbilityActivity.java
@@ -0,0 +1,117 @@
+package io.github.wly5556.s1orangeX;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import ohos.stage.ability.adapter.StageActivity;
+
+
+/**
+ * Example ace activity class, which will load ArkUI-X ability instance.
+ * StageActivity is provided by ArkUI-X
+ *
+ * @see <a href=
+ * "https://gitee.com/arkui-x/docs/blob/master/zh-cn/application-dev/tutorial/how-to-integrate-arkui-into-android.md">
+ * to build android library</a>
+ */
+public class EntryEntryAbilityActivity extends StageActivity {
+    private Bridge bridge;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        int currentNightMode = newConfig.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        bridge.callMethod("UiModeChanged", currentNightMode == Configuration.UI_MODE_NIGHT_YES);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        bridge = new Bridge(this, "Bridge", getBridgeManager());
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnApplyWindowInsetsListener((v, insets) -> {
+            int statusBarInset = insets.getSystemWindowInsetTop();
+            int navigationBarInset = insets.getSystemWindowInsetBottom();
+            bridge.callMethod("WindowInsetsListener", statusBarInset, navigationBarInset);
+            return insets;
+        });
+        setInstanceName("io.github.wly5556.s1orangeX:entry:EntryAbility:");
+        super.onCreate(savedInstanceState);
+    }
+
+    public static final int PICK_IMAGES_REQUEST_CODE = 1001;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGES_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                ArrayList<Uri> selectedImageUris = new ArrayList<>();
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                    int count = clipData.getItemCount();
+                    for (int i = 0; i < count; i++) {
+                        selectedImageUris.add(clipData.getItemAt(i).getUri());
+                    }
+                } else if (data.getData() != null) {
+                    selectedImageUris.add(data.getData());
+                }
+                new Thread(() -> {
+                    final ArrayList<String> cachedPaths = new ArrayList<>();
+
+                    for (Uri uri : selectedImageUris) {
+                        InputStream inputStream = null;
+                        FileOutputStream outputStream = null;
+                        try {
+                            inputStream = getContentResolver().openInputStream(uri);
+                            if (inputStream == null) continue;
+                            File tempFile = File.createTempFile("temp_image",
+                                    '.' + MimeTypeMap.getSingleton().getExtensionFromMimeType(
+                                            getContentResolver().getType(uri)
+                                    ), getCacheDir());
+                            tempFile.deleteOnExit();
+                            outputStream = new FileOutputStream(tempFile);
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+                            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                                outputStream.write(buffer, 0, bytesRead);
+                            }
+                            cachedPaths.add(tempFile.getAbsolutePath());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                if (inputStream != null) inputStream.close();
+                                if (outputStream != null) outputStream.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    runOnUiThread(() -> {
+                        if (bridge != null) {
+                            bridge.callMethod("PhotoPickerResult", cachedPaths.toArray());
+                        }
+                    });
+                }).start();
+            }
+        }
+    }
+}
---
+++ .arkui-x/android/app/src/main/java/io/github/wly5556/s1orangeX/MyApplication.java
@@ -0,0 +1,17 @@
+package io.github.wly5556.s1orangeX;
+
+import ohos.stage.ability.adapter.StageApplication;
+
+/**
+ * Example ace application class, which will load ArkUI-X application instance.
+ * StageApplication is provided by ArkUI-X
+ * @see <a href=
+ * "https://gitee.com/arkui-x/docs/blob/master/zh-cn/application-dev/tutorial/how-to-integrate-arkui-into-android.md">
+ * to build android library</a>
+ */
+public class MyApplication extends StageApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
